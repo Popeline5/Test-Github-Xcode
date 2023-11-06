f368a88c796416b5a0784ed06ef1648b85863e47
Pauline de Mortain <pauline.de.mortain1@gmail.com>
2023-11-06 14:55:14 +0100

    Initial Commit

---
+++ Test Github &  Xcode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test Github &  Xcode
+//
+//  Created by Pauline de Mortain on 2023-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
